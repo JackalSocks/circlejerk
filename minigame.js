5cbc75ab3a823f831505f276385b9c54e1ea81d3
JackalSocks <85052140+JackalSocks@users.noreply.github.com>
2025-07-30 19:47:08 -0700

    Update minigame.js

--- minigame.js
+++ minigame.js
@@ -36,8 +36,8 @@
 
   // start a new round
   function newRound() {
-    targetRadius   = 30 + Math.random() * 120;      // 30–150px
-    const shrinkDuration = 800 + Math.random() * 200; // 800–1200ms
+    targetRadius   = 20 + Math.random() * 120;      // 30–150px
+    const shrinkDuration = 400 + Math.random() * 200; // 800–1200ms
     const delay      = 500 + Math.random() * 1000;  // 0.5–1.5s before start
 
     shrinking = false;
